ceadbed1d8f09a37befc46768677b2ad0df200e7
kao <115608@o365.tku.edu.tw>
2020-07-23 14:02:59 +0800

    First Commit.

---
+++ Employee.cs
@@ -0,0 +1,8 @@
+namespace TkuOisAzFunc115608 {
+    public class Employee {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string EmpName { get; set; }
+        public string EmpEmail { get; set; }
+    }
+}
